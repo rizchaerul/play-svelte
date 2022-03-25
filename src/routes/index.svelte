f758590d8ed8396e460b26c69ca33772f740ccf5
Chaerul Rizky <riz.chaerul@gmail.com>
2022-03-25 16:17:42 +0700

    install bootstrap

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,7 +2,12 @@
     import "../app.scss";
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>
-    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<div class="d-flex justify-content-center align-items-center vh-100">
+    <div class="text-center">
+        <h1>Welcome to SvelteKit</h1>
+        <p>
+            Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the
+            documentation
+        </p>
+    </div>
+</div>
