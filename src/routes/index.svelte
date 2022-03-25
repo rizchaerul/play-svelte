79e499f8acd323ed8d34364cc138cf75e4c4bb09
Chaerul Rizky <riz.chaerul@gmail.com>
2022-03-25 16:13:51 +0700

    install sass

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,7 @@
+<script lang="ts">
+    import "../app.scss";
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>
     Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
