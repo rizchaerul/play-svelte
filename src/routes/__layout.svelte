70566b9ea9efb3a41128a20265dc3b1069e937cf
Chaerul Rizky <riz.chaerul@gmail.com>
2022-03-25 16:25:23 +0700

    fix bugs

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+    // TODO: Fix UI flicker on first load.
+    import "../app.scss";
+</script>
+
+<slot />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,13 +1,8 @@
-<script lang="ts">
-    import "../app.scss";
-</script>
-
 <div class="d-flex justify-content-center align-items-center vh-100">
     <div class="text-center">
         <h1>Welcome to SvelteKit</h1>
         <p>
-            Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the
-            documentation
+            Click <a href="/home">this</a> to go to the home page.
         </p>
     </div>
 </div>
