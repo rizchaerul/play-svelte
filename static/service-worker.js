186fe59a829b9ed425e5d4b8331734fe8a905da0
Chaerul Rizky <riz.chaerul@gmail.com>
2022-03-29 00:04:39 +0700

    add service worker

---
+++ static/service-worker.js
@@ -0,0 +1,5 @@
+// Minimal service worker for install button.
+// See https://stackoverflow.com/questions/61208907/pwa-minimal-service-worker-just-for-trigger-the-install-button
+self.addEventListener("fetch", () => {
+    return;
+});
